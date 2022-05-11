00cd575f029f202ae7e52d5bce474e6ccd384573
pavel <pavelstarshinov@gmail.com>
2022-05-12 00:47:00 +0300

    11.05 home

--- login/urls.py
+++ login/urls.py
@@ -8,17 +8,15 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')`
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('test_app.urls')),
-    path('', include('common.urls')),
-    path('', include('login.urls')),
+    path('login/', views.LoginVeiw.as_view()),
 ]
--- login/views.py
+++ login/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpRequest, HttpResponse, JsonResponse
 
-# Create your views here.
+
+class LoginVeiw(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'login/index.html')
+
+    def post(self, request):
+        return JsonResponse(request.post, json_dumps_params={'indent': 4})
